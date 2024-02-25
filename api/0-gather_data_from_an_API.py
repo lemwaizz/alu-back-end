c9fd637f44ab7c2e44fe4d684027c2888d2a1b61
lemwaizz <mwaidennis26@gmail.com>
2024-02-25 17:31:14 +0000

    fixes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,30 +2,50 @@
 """
 Retrieving data using apis
 """
-
-"""importing the right modules"""
+"""
+importing the right modules
+"""
 import requests 
 import sys
+import json
 
-"""passing id as the second argument."""
+
+"""
+passing id as the second argument.
+"""
 user_id = sys.argv[1]
 url1= "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-"""retrieve the data as json using requests method."""
+"""
+retrieve the data as json using requests method.
+"""
 resp1 = requests.get(url1, verify= False).json()
 
-"""get the value of the name key"""
+"""
+get the value of the name key
+"""
 name = resp1.get("name")
 url2= "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
 resp2 = requests.get(url2, verify= False).json()
 
-"""initialize count for completed tasks."""
+"""
+initialize count for completed tasks.
+"""
 count = 0
+"""
+loop through the resp2 json
+"""
 for todo in resp2:
 	if todo["completed"]:
 		count += 1
 		total_tasks = len(resp2)
+"""
+print all outputs
+"""
 print("Employee {} is done with tasks({}/{}):".format(name, count, total_tasks))
+"""
+loop through resp2 again finding the title and print it
+"""
 for todo in resp2:
 	if todo["completed"]:
 		title = resp2[int(user_id)-1].get("title")
