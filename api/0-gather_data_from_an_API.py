3b4eda82c5bc55c155d8c6cf212c2c0c52c2103b
lemwaizz <mwaidennis26@gmail.com>
2024-02-25 18:49:36 +0000

    finished

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,49 +2,50 @@
 """
     python script that returns TODO list progress for a given employee ID
 """
-import json
-import requests
-import sys
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
 
+    """
+       passing id as the second argument.
+    """
+    user_id = sys.argv[1]
+    url1 = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-"""
-   passing id as the second argument.
-"""
-user_id = sys.argv[1]
-url1 = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-
-"""
-   retrieve the data as json using requests method.
-"""
-resp1 = requests.get(url1, verify=False).json()
-"""
-   get the value of the name key
-"""
-name = resp1.get("name")
-url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
-"""
-   convert to json.
-"""
-resp2 = requests.get(url2, verify=False).json()
-"""
-   initialize count for completed tasks.
-"""
-count = 0
-"""
-   loop through the resp2 json while increasing count of completed tasks.
-"""
-for todo in resp2:
-    if todo["completed"]:
-        count += 1
-        total = len(resp2)
-"""
-   print all outputs
-"""
-print("Employee {} is done with tasks({}/{}):".format(name, count, total))
-"""
-   loop through resp2 again finding the title and print it
-"""
-for todo in resp2:
-    if todo["completed"]:
-        title = resp2[int(user_id)-1].get("title")
-        print("	{}".format(title))
+    """
+       retrieve the data as json using requests method.
+    """
+    resp1 = requests.get(url1, verify=False).json()
+    """
+       get the value of the name key
+    """
+    name = resp1.get("name")
+    url_raw = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    url2 = url_raw.format(user_id)
+    """
+       convert to json.
+    """
+    resp2 = requests.get(url2, verify=False).json()
+    """
+       initialize count for completed tasks.
+    """
+    count = 0
+    """
+       loop through the resp2 json while increasing count of completed tasks.
+    """
+    for todo in resp2:
+        if todo["completed"]:
+            count += 1
+            total = len(resp2)
+    """
+       print all outputs
+    """
+    print("Employee {} is done with tasks({}/{}):".format(name, count, total))
+    """
+       loop through resp2 again finding the title and print it
+    """
+    for todo in resp2:
+        if todo["completed"]:
+            title = resp2[int(user_id)-1].get("title")
+            print(" {}".format(title))
