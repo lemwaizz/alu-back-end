baa38c38e4fa5a94663f376b322e05efe12882e0
lemwaizz <mwaidennis26@gmail.com>
2024-02-25 20:41:06 +0000

    fixed

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     url_raw = "https://jsonplaceholder.typicode.com/users/{}/todos"
     url1 = url_raw.format(user_id)
     resp1 = requests.get(url1, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/{}"
+    url = "https://jsonplaceholder.typicode.com/users/{}"
     url2 = url.format(user_id)
     resp2 = requests.get(url2, verify=False).json()
-    filename = f"{sys.argv[1]}.csv"
-    with open(filename, 'w', newline='') as csvfile:
+    with open('{}.csv'.format(sys.argv[1]), mode='w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=",", quotechar='"',
                                 quoting=csv.QUOTE_ALL)
-    for data in resp1:
-        csv_writer.writerow([data["userId"], resp2.get("username"),
-                            data["completed"], data["title"]])
+        for data in resp1:
+            csv_writer.writerow([data["userId"],
+                                 resp2.get("username"),
+                                 data["completed"], data["title"]])
